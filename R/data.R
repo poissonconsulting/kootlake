d248e12ffe70f41a0848dd70290f156be3da1c03
Joe Thorley <joe@poissonconsulting.ca>
2016-07-01 07:46:44 -0700

    documentation

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Gerrard Spawner Counts
 #'
-#' The Gerrard Rainbow Trout spawner counts from 1961 to 2014.
+#' The Gerrard Rainbow Trout spawner counts from 1961 to 2015.
 #'
 #' The total escapement can be estimated by multiplying the peak count by 3.08
 #' or by dividing the total number of fish days by the residence time (currently)
@@ -10,8 +10,8 @@
 #' \describe{
 #'   \item{Year}{The year of the spawner count as an integer.}
 #'   \item{PeakCount}{The peak count as an integer.}
-#'   \item{FishDays}{The total number of fish days as estimated by AUC assuming
-#'   100 observer efficiency as an integer.}
+#'   \item{FishDays}{The total number of fish days (as estimated by AUC assuming
+#'   100 percent observer efficiency) as an integer.}
 #'}
 #' @seealso \code{\link{kootlake}}.
 "gerrard"
