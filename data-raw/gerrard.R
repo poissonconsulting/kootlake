1833464823b8ee54b40f30db2f5838caa08df03c
Joe Thorley <joe@poissonconsulting.ca>
2016-07-08 09:06:04 -0700

    rename Gerrard_Escapement-Estimate.csv as gerrard.csv

--- data-raw/gerrard.R
+++ data-raw/gerrard.R
@@ -2,6 +2,8 @@ library(dplyr)
 library(magrittr)
 library(devtools)
 
-gerrard <- read.csv("data-raw/Gerrard-Escapement-Estimate.csv")
+rm(list = ls())
+
+gerrard <- read.csv("data-raw/gerrard.csv")
 gerrard %<>% arrange(Year) %>% select(Year, PeakCount, FishDays)
 devtools::use_data(gerrard, overwrite = TRUE)
