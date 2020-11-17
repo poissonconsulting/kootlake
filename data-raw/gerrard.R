72c18441081cbc87ddf351f384554bef3c0dbddb
Joe Thorley <joe@poissonconsulting.ca>
2020-11-16 17:24:12 -0800

    gerrard from 1961

--- data-raw/gerrard.R
+++ data-raw/gerrard.R
@@ -6,7 +6,10 @@ rm(list = ls())
 
 gerrard <- read_csv("data-raw/gerrard.csv")
 
-gerrard %<>% arrange(Year) %>% select(Year, PeakCount, FishDays) %>%
+gerrard %<>%
+  filter(Year >= 1961) %>%
+  arrange(Year) %>%
+  select(Year, PeakCount, FishDays) %>%
   mutate(Year = as.integer(Year), PeakCount = as.integer(PeakCount), FishDays = as.integer(FishDays))
 
 usethis::use_data(gerrard, overwrite = TRUE)
