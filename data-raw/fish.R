b8ee13221129fed6fe6c867e12e10d6d627174fb
Evan <evanamiesgalonski@gmail.com>
2021-05-28 15:24:30 -0700

    data raw

--- data-raw/fish.R
+++ data-raw/fish.R
@@ -8,4 +8,12 @@ rm(list = ls())
 fish <- read_csv("data-raw/fish.csv")
 fish %<>% arrange(Year)
 
-devtools::use_data(fish, overwrite = TRUE)
+fish2020 <- read_csv("data-raw/fish2020.csv")
+fish2020 %<>% arrange(Year) %>%
+  mutate(Sex = case_when(Sex == "m" ~ "Male",
+                         Sex == "f" ~ "Female"),
+         Length = Length * 10)
+
+fish %<>% bind_rows(fish2020)
+
+usethis::use_data(fish, overwrite = TRUE)
