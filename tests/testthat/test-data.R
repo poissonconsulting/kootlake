f8b0353f0a758421f3d21dd4ab4c683b18e2f00e
Evan <evanamiesgalonski@gmail.com>
2021-05-31 09:20:52 -0700

    fix tests again for actions

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,40 +1,37 @@
 test_that("bulltrout", {
- expect_identical(chk::check_data(kootlake::bulltrout, values = list(
+ expect_invisible(chk::check_data(kootlake::bulltrout, values = list(
     Year = c(1964L, 2020L),
     KasloRedds = c(100L, 2000L, NA),
     KeenRedds = c(10L, 500L, NA),
     KasloCounter = c(100L, 2000L, NA)),
     exclusive = TRUE,
     order = TRUE,
-    key = "Year"),
-    kootlake::bulltrout)
+    key = "Year"))
 })
 
 test_that("fishery", {
- expect_identical(chk::check_data(kootlake::fishery, values = list(
+ expect_invisible(chk::check_data(kootlake::fishery, values = list(
     StartYear = c(1980L, 2020L),
     EndYear = c(1980L, 2020L),
     AnglerHours = c(0, 1000000, NA)),
     exclusive = TRUE,
     order = TRUE,
-    key = "StartYear"),
-    kootlake::fishery)
+    key = "StartYear"))
 })
 
 test_that("gerrard", {
- expect_identical(chk::check_data(kootlake::gerrard, values = list(
+ expect_invisible(chk::check_data(kootlake::gerrard, values = list(
     Year = c(1957L, 2020L),
     PeakCount = c(0L, 1500L),
     FishDays = c(0L, 20000L, NA)
     ),
     exclusive = TRUE,
     order = TRUE,
-    key = "Year"),
-    kootlake::gerrard)
+    key = "Year"))
 })
 
 test_that("kokanee", {
- expect_identical(chk::check_data(kootlake::kokanee, values = list(
+ expect_invisible(chk::check_data(kootlake::kokanee, values = list(
     Year = c(1964L, 2020L),
     Lardeau = c(1000L, 5000000L, NA),
     MeadowCreek = c(1000L, 3000000L),
@@ -44,6 +41,5 @@ test_that("kokanee", {
     ),
     exclusive = TRUE,
     order = TRUE,
-    key = "Year"),
-    kootlake::kokanee)
+    key = "Year"))
 })
