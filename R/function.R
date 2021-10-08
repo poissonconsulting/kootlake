45478a1c16ecf98d2db08dbf23569f21b32049b2
Evan <evanamiesgalonski@gmail.com>
2021-10-08 11:20:08 -0700

    try adding function to fix coverage https://github.com/r-lib/covr/issues/427

---
+++ R/function.R
@@ -0,0 +1,18 @@
+#' temp_function
+#'
+#' function included as workaround for issue:
+#' https://github.com/r-lib/covr/issues/427
+#'
+#' @param x Any object
+#'
+#' @return The same object.
+#' @export
+#'
+#' @examples
+#'
+#' temp_function(3)
+#'
+
+temp_function <- function(x) {
+  x
+}
