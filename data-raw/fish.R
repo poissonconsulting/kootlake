df03d67a18fa8f48677aa403a35d299627a4ff74
Evan <evanamiesgalonski@gmail.com>
2022-06-06 15:38:01 -0700

    add partially RB data set from 2021

--- data-raw/fish.R
+++ data-raw/fish.R
@@ -8,12 +8,12 @@ rm(list = ls())
 fish <- read_csv("data-raw/fish.csv")
 fish %<>% arrange(Year)
 
-fish2020 <- read_csv("data-raw/fish2020.csv")
-fish2020 %<>% arrange(Year) %>%
+fish_recent <- read_csv("data-raw/fish_recent.csv")
+fish_recent %<>% arrange(Year) %>%
   mutate(Sex = case_when(Sex == "m" ~ "Male",
                          Sex == "f" ~ "Female"),
          Length = Length * 10)
 
-fish %<>% bind_rows(fish2020)
+fish %<>% bind_rows(fish_recent)
 
 usethis::use_data(fish, overwrite = TRUE)
