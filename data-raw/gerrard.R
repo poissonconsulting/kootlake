fd73666dfe566cd6f0c217f3a218a3530c9b5367
Evan <evanamiesgalonski@gmail.com>
2025-05-06 15:58:02 -0700

    data work up for gerrard data

--- data-raw/gerrard.R
+++ data-raw/gerrard.R
@@ -4,20 +4,23 @@ library(readr)
 library(usethis)
 rm(list = ls())
 
-gerrard <- read_csv("data-raw/gerrard.csv")
+fish_removed <- readxl::read_excel("data-raw/GerrardRemoved.xlsx")
 
-gerrard %<>%
-  filter(Year >= 1961) %>%
-  arrange(Year) %>%
-  select(Year, PeakCount, FishDays) %>%
+gerrard <- readxl::read_excel(
+  "data-raw/Gerrard data for Evan 2024.xlsx",
+  sheet = "Spawner count",
+  skip = 15
+) %>%
+  suppressMessages() %>%
+  select(
+    Year = `...1`, PeakCount = count, FishDays = `(fish*days)`
+  ) %>%
+  left_join(fish_removed, by = "Year") %>%
   mutate(
-    Year = as.integer(Year),
     PeakCount = as.integer(PeakCount),
-    FishDays = as.integer(FishDays),
-    FishRemoved = case_when(
-      Year == 2022 ~ 150,
-      TRUE ~ 0
-      )
-    )
+    FishRemoved = if_else(is.na(FishRemoved), 0L, as.integer(FishRemoved)),
+    Year = as.integer(Year)
+  ) %>%
+  arrange(Year)
 
 usethis::use_data(gerrard, overwrite = TRUE)
